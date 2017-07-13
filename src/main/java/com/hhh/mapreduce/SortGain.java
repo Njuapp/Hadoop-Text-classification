5221e5abd273c4028b56a4c606241c5d0524e69a
chenghang <hangcheng_1997@126.com>
2017-07-13 19:54:36 +0800

    Sort of Information Entropy

---
+++ src/main/java/com/hhh/mapreduce/SortGain.java
@@ -0,0 +1,32 @@
+package com.hhh.mapreduce;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
+/**
+ * Created by joker on 17-7-13.
+ */
+public class SortGain {
+    public static void main(String[] args)throws IOException, InterruptedException, ClassNotFoundException{
+        if(args.length != 2){
+            System.err.println("Usage:InfoGain <input><output>");
+            System.exit(2);
+        }
+
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "SortGain");
+        job.setJarByClass(SortGain.class);
+        job.setInputFormatClass(KeyValueTextInputFormat.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(Text.class);
+        KeyValueTextInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        job.waitForCompletion(true);
+    }
+}
